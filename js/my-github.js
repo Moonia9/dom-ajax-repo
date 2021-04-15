9c48ccfcf0b4fe89cdda82c17489f719aa651b1e
Enia Munteanu <75390874+Moonia9@users.noreply.github.com>
2021-04-15 13:12:11 +0200

    updated my-github.js/Requirement 1

--- js/my-github.js
+++ js/my-github.js
@@ -7,10 +7,8 @@ const getReposGithub = `https://api.github.com/users/${myUser}/repos`;
 function fetchGithubLink(api) {
   const getRepos = `${api}`;
   fetch(getRepos)
-    .then(function(response) {
-      return response.json();
-    })
-    .then(function(repos) {
+    .then(response => response.json())
+    .then(repos => {
       console.log(repos);
       const reposList = document.querySelector("#repos-list");
       const reposArray = Array.from(repos);
@@ -26,21 +24,6 @@ function fetchGithubLink(api) {
         reposList.appendChild(li);
       });
       return repos;
-
-      //   for (let i = 0; i < repos.length; i++) {
-      //     const repo = repos[i];
-      //     const repoElement = document.createElement("li");
-      //     let a = document.createElement("a");
-      //     a.setAttribute("href", repo.html_url);
-      //     a.innerHTML = repo.name;
-      //     repoElement.appendChild(a);
-      //     reposList.appendChild(repoElement);
-      //     let counter = document.getElementsById("repos-count");
-      //     counter.innerHTML = repos.length;
-      //     li.appendChild(a);
-      //     counter.appendChild(li);
-      //   }
-      //   return repos;
     });
 }
 
